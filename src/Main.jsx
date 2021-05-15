59b394e40484da06accb58ee861f84fa41e3d0bd
Jordan Arldt <jordan.arldt@bigcommerce.com>
2021-05-15 08:23:21 -0500

    Update Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -7,7 +7,7 @@ export default class Main extends React.Component {
       <div className="container">
         <h1>Embedded Checkout Test</h1>
         <p>This is to test the BigCommerce Embedded Checkout with the Checkout-SDK</p>
-        <EmbeddedCheckout cartId="4e53123a-ea49-4676-8973-26d57cf391bf" />
+        <EmbeddedCheckout />
       </div>
     );
   }
